c9cdd3c8251ef4e0d1a085536a7996211f7c2846
kin <e19950320@gmail.com>
2015-03-26 11:18:22 +0800

    Authorization on links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,6 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, :except => [:index, :show]
 
   # GET /links
   # GET /links.json
@@ -15,6 +16,7 @@ class LinksController < ApplicationController
   # GET /links/new
   def new
     @link = Link.new
+    @link = current_user.links.build
   end
 
   # GET /links/1/edit
@@ -25,7 +27,7 @@ class LinksController < ApplicationController
   # POST /links.json
   def create
     @link = Link.new(link_params)
-
+    @link = current_usr.links.build(link_params)
     respond_to do |format|
       if @link.save
         format.html { redirect_to @link, notice: 'Link was successfully created.' }
