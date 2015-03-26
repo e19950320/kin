a42f792705f522e6224417f5bbd87a176e8b0b27
kin <e19950320@gmail.com>
2015-03-26 11:23:46 +0800

    Add structure & basic styling

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem'coffee-script-source','1.8.0'
 gem 'devise','~> 3.3.0'
+gem 'bootstrap-sass', '~> 3.2.0.2'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.8'
 # Use sqlite3 as the database for Active Record
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,7 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!, :except => [:index, :show]
+  before_action :authorized_user, only: [:edit, :update, :destroy]
 
   # GET /links
   # GET /links.json
@@ -73,4 +74,8 @@ class LinksController < ApplicationController
     def link_params
       params.require(:link).permit(:title, :url)
     end
-end
+
+  def authorized_user
+    @link = current_user.links.find_by(id: params[:id])
+    redirect_to links_path, notice: "Not authorized to edit this link" if @link.nil?
+    end
